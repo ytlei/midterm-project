4545127dc05b6d269d9b7014f06fe71cf2d1407d
ytlei <ytlei@umd.edu>
2017-03-18 17:34:11 -0500

    add lqrControl.cpp

---
+++ app/lqrControl.cpp
@@ -0,0 +1,33 @@
+/************************************************************
+ *                                                          *
+ * Copyright (C) 2017 by Yi-ting Lei                        *
+ *                                                          *
+ ***********************************************************/
+
+/**
+ *   @file	lqrControl.cpp
+ *   @brief  	This is the main file to run the LQR controller
+ *      
+ *   convert the input to state space representation. Do the LQR control
+ *
+ *   @author	Yi-ting Lei
+ *   @date	2017/3/17
+ */
+
+#include <iostream>
+//#include <lib.hpp>
+#include <lqrControl.hpp>
+
+
+bool lqrControl::convertStateSpace() { 	///< set the pid parameters
+
+
+
+}
+		
+double lqrControl::lqr(double, double, double){	///< show current velocity
+
+
+
+}
+
