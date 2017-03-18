e4cc4ed5de5569e7f8a8d1e8aadda4d9e2384130
ytlei <ytlei@umd.edu>
2017-03-18 17:10:31 -0500

    edit carControl.cpp

---
+++ app/ carControl.cpp
@@ -0,0 +1,40 @@
+/************************************************************
+ *                                                          *
+ * Copyright (C) 2017 by Yi-ting Lei                        *
+ *                                                          *
+ ***********************************************************/
+
+/**
+ *   @file	carControl.cpp
+ *   @brief  	This is the main file to run the car control
+ *      
+ *   Set the desired velocity. Using ccelerate and brake to control the car
+ *
+ *   @author	Yi-ting Lei
+ *   @date	2017/3/17
+ */
+
+#include <iostream>
+//#include <lib.hpp>
+#include <carControl.hpp>
+
+
+
+double carControl::getSpeed(){
+	return speed;
+}
+
+void carControl::setSpeed(double speedSet){
+	speed = speedSet;
+}
+		
+
+void carControl::accelerate(){
+	speed += 1;
+}
+		
+void carControl::brake(){
+	speed -= 1;
+}
+
+
---
+++ include/carControl.hpp~
@@ -0,0 +1,34 @@
+/************************************************************
+ *                                                          *
+ * Copyright (C) 2017 by Yi-ting Lei                        *
+ *                                                          *
+ ***********************************************************/
+
+/**
+ *   @file	carControl.hpp
+ *   @brief  	This is a car controller for controlling car speed.
+ *		one can set target velocity and get current speed.
+ *      
+ *   Control the velocity using brake and acceleration
+ *
+ *   @author	Yi-ting Lei
+ *   @date	2017/3/12
+ */
+#ifndef INCLUDE_CARCONTROL_HPP_
+#define INCLUDE_CARCONTROL_HPP_
+
+class carControl {
+
+		double speed;
+		double targetVelocity;
+
+	public:
+		
+		double getSpeed();
+		void setSpeed();
+		void accelerate();
+		void brake();
+	
+};
+
+#endif
