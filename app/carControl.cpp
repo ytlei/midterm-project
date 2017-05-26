a9789d3c46b03319d87e1b66222a3d27ef9b31c8
ytlei <ytlei@umd.edu>
2017-05-26 10:06:44 -0500

    add car control and car

--- app/carControl.cpp
+++ app/carControl.cpp
@@ -18,7 +18,97 @@
 //#include <lib.hpp>
 #include <carControl.hpp>
 
+#include <iostream>
+#include <tuple>
+#include <pidController.hpp>
 
+/**
+    
+ *   @brief  Constructor function of
+ *           PIDController
+ *
+ *   @param  none
+ *   @return none
+ */
+pidController::pidController(void) {}
+
+
+/**
+    
+ *   @brief  Destructor function of
+ *           PIDController
+ *
+ *   @param  none
+ *   @return none
+ */
+
+pidController::~pidController(void) {}
+
+
+/**
+   The function takes sets the gain values of the PID controller.
+   @param[in]     &p Proportional gain.
+   @param[in]     &i Integral gain.
+   @param[in]     &d Differential gain.
+   @return Function is of type Void.
+ */
+void pidController::setParams(double &p, double &i, double &d) {
+    pidController::Kp = p;
+    pidController::Ki = i;
+    pidController::Kd = d;
+}
+
+
+/**
+   The function takes two strings and their lengths as input.
+   It returns the location of second string in the first string as an integer.
+  
+   @param     none
+   @return std::tuple<double, double, double>.
+ */
+std::tuple<double, double, double> pidController::getParams() {
+    return std::make_tuple(pidController::Kp, pidController::Ki,
+                           pidController::Kd);
+}
+
+
+/**
+   The function takes the current value, target value and time difference as inputs and returns Control value 
+  
+   @param[in]     &setVal Target value.
+   @param[in]     &currentVal Current value.
+   @param[in]     &time Time difference.
+   @return        newVal Control value.
+ */
+double pidController::computeControl(double &setVal, double &currentVal,
+                                     double &time) {
+    double newVal = 0;
+    double error = 0;
+    double prevError = 0;
+    double integral = 0;
+    double derivative = 0;
+
+    // diff time should not be zero
+    if (time == 0)
+        return 0;
+
+    error = setVal - currentVal;
+    integral = integral + error * time;
+    derivative = (error - prevError) / time;
+
+    newVal = Kp * error + Ki * integral  + Kd * derivative;
+    prevError = error;
+
+    return newVal;
+}
+
+
+
+
+
+
+
+//================================
 double carControl::getSpeed(){
 	return speed;
 }
--- readme.md
+++ readme.md
@@ -5,15 +5,57 @@
 
 ## Overview
 
-Simple starter C++ project with:
+This project simulates the cruise control system on automobiles using control algorithms. It will also implements a simple unit test program which utilize googletest framework to 
+perform tests on module function. Also, the implementation process of this project follows SIP model in software enginering.
 
-- cmake
-- googletest
+Backlogs can be found at [BackLogs][reference-id-for-backlogs].
 
-## Installation
+[reference-id-for-backlogs]: 
+https://docs.google.com/spreadsheets/d/1zvV9tH2fySRXZiNTsEiNcHox-10YE9jk1I9JvUUO2zs/edit?usp=sharing
+UML class/activity diagrams can be found in git under ./UML folder.
+
+
+
+## License
+
+This program is under MIT License.
+
+Copyright (c) 2017 Yi-ting Lei
+```bash
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
+
+
+## Dependencies
+
+None
+
+
+## How to build
 
 - Checkout the repo (and submodules)
-```
+```bash
 $ git clone --recursive https://github.com/ytlei/midterm-project.git
+cd path
+mkdir -p build
+cd build
+cmake ..
+make
 ```
 
